574bca1cda43f45ea04b50e0abbf3f524434b7cb
namancg <naman.c@perfios.com>
2021-08-21 01:18:14 +0530

    ADDED: uc2-Equality

--- src/com/bridgelabz/linecomparision/LineComparision.java
+++ src/com/bridgelabz/linecomparision/LineComparision.java
@@ -5,14 +5,32 @@ public class LineComparision {
 	public static double getLength(int x1,int x2,int y1, int y2)
 	{
 		double result;
-		result = Math.sqrt((x2-x1)^2+(y2-y1)^2);
+		double val = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
+				//System.out.println(val);
+		result = Math.sqrt(val);
 		return result;
 	}
+	
+	
 	public static void main(String [] args) 
 	{
+		int x1 = (int)(Math.random()*100);
+		int x2 = (int)(Math.random()*100);
+		int y1 = (int)(Math.random()*100);
+		int y2 = (int)(Math.random()*100);
+		int x11 = (int)(Math.random()*100);
+		int x12 = (int)(Math.random()*100);
+		int y11 = (int)(Math.random()*100);
+		int y12 = (int)(Math.random()*100);
+		
+		Double l1 = getLength(x1,x2,y1,y2);
+		Double l2 = getLength(x11,x12,y11,y12);
+		System.out.println(l1);
+		System.out.println(l2);
 		System.out.println("Welcome to line comparision computation program");
-		System.out.println("the length of the lines is:" +getLength(10,12,7,8));
-	}
-	}
-
-
+		System.out.println("if l1 equal to l2? " +l1.equals(l2));
+		int val=l1.compareTo(l2);
+		if(val ==0)
+			System.out.println("both values are equal");
+}
+}
